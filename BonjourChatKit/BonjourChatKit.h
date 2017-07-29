3d20d34ca005d3a5afebcb9e703c60b9b48fa4db
M Mehrabi <miladsp2@gmail.com>
2017-07-29 14:13:06 -0700

    Initial Commit. FW Setup for iOS/macOS

---
+++ BonjourChatKit/BonjourChatKit.h
@@ -0,0 +1,23 @@
+//
+//  BonjourChatKit.h
+//  BonjourChatKit
+//
+//  Created by work on 7/29/17.
+//  Copyright © 2017 MoeMilMeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Useful Note:
+//
+// FW for iOS/MacOS: http://colemancda.github.io/2015/02/11/universal-ios-osx-framework
+
+//! Project version number for BonjourChatKit.
+FOUNDATION_EXPORT double BonjourChatKitVersionNumber;
+
+//! Project version string for BonjourChatKit.
+FOUNDATION_EXPORT const unsigned char BonjourChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BonjourChatKit/PublicHeader.h>
+
+
