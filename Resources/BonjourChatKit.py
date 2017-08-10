9a98290487a2df06a712fd0bbce9c760b39ed625
M Mehrabi <miladsp2@gmail.com>
2017-08-09 19:23:03 -0700

    Added bridging support and Library wrapper for PyObjC

---
+++ Resources/BonjourChatKit.py
@@ -0,0 +1,53 @@
+"""
+    BonjourChatKit.h
+    BonjourChatKit
+    
+    Created by MMM on 8/9/17.
+    Copyright (c) 2017 MoeMilMeh. All rights reserved.
+"""
+
+import objc
+import os
+import sys
+
+#######################################################################
+# Function to load the bundle for creating a library wrapper
+#
+# Source: https://pythonhosted.org/pyobjc/metadata/bridgesupport.html
+#
+#######################################################################
+
+def loadBundle(frameworkPath):
+    
+    if frameworkPath != None:
+
+        framework = 'BonjourChatKit.framework'
+        identifier = 'com.MoeMilMeh.BonjourChatKit'
+        if framework in frameworkPath:
+            if os.path.exists(frameworkPath):
+                __bundle__ = objc.initFrameworkWrapper(framework, frameworkIdentifier=identifier, frameworkPath=objc.pathForFramework(frameworkPath), globals=globals())
+                return True
+
+    return False
+
+frameworkPath = None
+argumentsCount = len(sys.argv)
+
+if argumentsCount > 2:
+    print "\nUsage: python " + sys.argv[0] + " PathToFramework \n"
+    sys.exit()
+
+elif argumentsCount == 2:
+    frameworkPath = sys.argv[1]
+
+else:
+    frameworkPath = '/Library/Frameworks/BonjourChatKit.framework'
+    print "Using default frameworkPath: " + frameworkPath
+
+# Check if bundle is loaded
+if loadBundle(frameworkPath):
+    print "Successfully loaded BonjourChatKit"
+else:
+    print "ERROR: Failed to load BonjourChatKit from " + frameworkPath
+
+del objc, os, sys
---
+++ Resources/PythonBridgeScriptSetup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#  PythonBridgeScriptSetup.sh
+#  BonjourChatKit
+#
+#  Created by Moe Mehrabi on 8/9/17.
+#  Copyright © 2017 MoeMilMeh. All rights reserved.
+
+SCRIPT_PATH="/Library/Frameworks/BonjourChatKit.framework/Versions/Current/Resources/"
+INSTALL_PATH=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
+sudo rm -rf "${INSTALL_PATH}/BonjourChatKit.py"
+sudo ln -fs "${SCRIPT_PATH}/BonjourChatKit.py" "${INSTALL_PATH}/BonjourChatKit.py"
+echo "Python installed BonjourChatKit in '${INSTALL_PATH}'"
+
